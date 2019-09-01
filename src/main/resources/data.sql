7af750d138cad399650b242725701f107838a4d7
Marcelo <m_campos_19@hotmail.com>
2019-09-01 16:32:41 -0300

    #3

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$lug6vhd/SEnsZH4I1Q8eH.zaXyJg6hHWvou7Oa1jPyPmwPrZn09WC');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
